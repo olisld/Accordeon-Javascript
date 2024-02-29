24543e250896d11649c8072fc213b695e255dada
olisld <olivier.soulard4@gmail.com>
2024-02-29 17:01:14 +0100

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const accordions = Array.from(document.querySelectorAll(".accordion"));
-
+const accordions = Array.from(document.querySelectorAll(".accordion")); 
 accordions.map((accordion) => {
+  accordion.addEventListener('click',function(){
+    accordion.classList.toggle('active');
+    if(accordion.classList.contains('active')){
+     accordion.nextElementSibling.style.display = 'block';
+    }
+    else{
+      accordion.nextElementSibling.style.display = 'none';
+    }
+  })
   // Code
 });
